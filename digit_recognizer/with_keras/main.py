7bbc9399957fe893b872e8bd6c3ccb7f77a9f749
cracr <cracroule@gmail.com>
2018-01-30 19:04:16 +0100

    add error analysis

--- digit_recognizer/with_keras/main.py
+++ digit_recognizer/with_keras/main.py
@@ -17,7 +17,7 @@ from keras.callbacks import ReduceLROnPlateau
 
 np.random.seed(2)
 
-DISPLAY = False
+DISPLAY = True
 
 
 def main():
@@ -59,33 +59,58 @@ def main():
                                                 factor=0.5,
                                                 min_lr=0.00001)
 
-    epochs = 20  # Turn epochs to 30 to get 0.9967 accuracy
-    batch_size = 128
+    epochs = 15  # Turn epochs to 30 to get 0.9967 accuracy
+    batch_size = 64
 
-    # # With data augmentation to prevent overfitting (accuracy 0.99286)
-    # datagen = ImageDataGenerator(
-    #     featurewise_center=False,  # set input mean to 0 over the dataset
-    #     samplewise_center=False,  # set each sample mean to 0
-    #     featurewise_std_normalization=False,  # divide inputs by std of the dataset
-    #     samplewise_std_normalization=False,  # divide each input by its std
-    #     zca_whitening=False,  # apply ZCA whitening
-    #     rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
-    #     zoom_range=0.1,  # Randomly zoom image
-    #     width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-    #     height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-    #     horizontal_flip=False,  # randomly flip images
-    #     vertical_flip=False)  # randomly flip images
-    #
-    # datagen.fit(X_train)
-    #
-    # # Fit the model
-    # history = model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size),
-    #                               epochs = epochs, validation_data=(X_val, Y_val),
-    #                               verbose = 2, steps_per_epoch=X_train.shape[0] // batch_size
-    #                               , callbacks=[learning_rate_reduction])
+    # With data augmentation to prevent overfitting (accuracy 0.99286)
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range=0.1,  # Randomly zoom image
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
 
-    history = model.fit(x=X_train, y=Y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, Y_val),
-                        verbose=2, callbacks=[learning_rate_reduction])
+    datagen.fit(X_train)
+
+    # Fit the model
+    history = model.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size),
+                                  epochs = epochs, validation_data=(X_val, Y_val),
+                                  verbose = 2, steps_per_epoch=X_train.shape[0] // batch_size
+                                  , callbacks=[learning_rate_reduction])
+
+    # easier version below, without data augmentation
+    # history = model.fit(x=X_train, y=Y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, Y_val),
+    #                     verbose=2, callbacks=[learning_rate_reduction])
+
+    # Plot the loss and accuracy curves for training and validation
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(history.history['loss'], color='b', label="Training loss")
+    ax[0].plot(history.history['val_loss'], color='r', label="validation loss", axes=ax[0])
+    legend = ax[0].legend(loc='best', shadow=True)
+
+    ax[1].plot(history.history['acc'], color='b', label="Training accuracy")
+    ax[1].plot(history.history['val_acc'], color='r', label="Validation accuracy")
+    legend = ax[1].legend(loc='best', shadow=True)
+    if DISPLAY: plt.show()
+
+    # Predict the values from the validation dataset
+    Y_val_pred = model.predict(X_val)
+    # Convert predictions classes to one hot vectors
+    Y_pred_classes = np.argmax(Y_val_pred, axis=1)
+    # Convert validation observations to one hot vectors
+    Y_true = np.argmax(Y_val, axis=1)
+    # compute the confusion matrix
+    confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
+    # plot the confusion matrix
+    plot_confusion_matrix(confusion_mtx, classes=range(10))
+
+    error_analysis(X_val, Y_true, Y_val_pred, Y_pred_classes)
 
     # predict results
     results = model.predict(test)
@@ -98,16 +123,6 @@ def main():
     submission = pd.concat([pd.Series(range(1, 28001), name="ImageId"), results], axis=1)
 
     submission.to_csv("cnn_mnist_datagen.csv", index=False)
-
-    # Plot the loss and accuracy curves for training and validation
-    fig, ax = plt.subplots(2, 1)
-    ax[0].plot(history.history['loss'], color='b', label="Training loss")
-    ax[0].plot(history.history['val_loss'], color='r', label="validation loss", axes=ax[0])
-    legend = ax[0].legend(loc='best', shadow=True)
-
-    ax[1].plot(history.history['acc'], color='b', label="Training accuracy")
-    ax[1].plot(history.history['val_acc'], color='r', label="Validation accuracy")
-    legend = ax[1].legend(loc='best', shadow=True)
 
 
 def pre_analyze_data(X_train, Y_train, test):
@@ -173,6 +188,81 @@ def prepare_model():
     model.add(Dense(10, activation="softmax"))
 
     return model
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    if DISPLAY: plt.show()
+
+
+def display_errors(errors_index, img_errors, pred_errors, obs_errors):
+    """ This function shows 6 images with their predicted and real labels"""
+    n = 0
+    nrows = 2
+    ncols = 3
+    fig, ax = plt.subplots(nrows, ncols, sharex=True, sharey=True)
+    for row in range(nrows):
+        for col in range(ncols):
+            error = errors_index[n]
+            ax[row, col].imshow((img_errors[error]).reshape((28, 28)))
+            ax[row, col].set_title(
+                "Predicted label :{}\nTrue label :{}".format(pred_errors[error], obs_errors[error]))
+            n += 1
+    if DISPLAY: plt.show()
+
+
+def error_analysis(X_val, Y_true, Y_val_pred, Y_pred_classes):
+
+    # Errors are difference between predicted labels and true labels
+    errors = (Y_pred_classes - Y_true != 0)
+
+    Y_pred_classes_errors = Y_pred_classes[errors]
+    Y_pred_errors = Y_val_pred[errors]
+    Y_true_errors = Y_true[errors]
+    X_val_errors = X_val[errors]
+
+    # Probabilities of the wrong predicted numbers
+    Y_pred_errors_prob = np.max(Y_pred_errors, axis=1)
+
+    # Predicted probabilities of the true values in the error set
+    true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
+
+    # Difference between the probability of the predicted label and the true label
+    delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
+
+    # Sorted list of the delta prob errors
+    sorted_dela_errors = np.argsort(delta_pred_true_errors)
+
+    # Top 6 errors
+    most_important_errors = sorted_dela_errors[-6:]
+
+    # Show the top 6 errors
+    display_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
 
 if __name__ == '__main__':
     main()
