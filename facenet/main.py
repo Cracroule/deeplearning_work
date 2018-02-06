2fe2694c6e948103e88be5ef627ef98768161f8d
cracr <cracroule@gmail.com>
2018-02-06 15:20:53 +0100

    correct error in dist computation (see comments in code)

--- facenet/main.py
+++ facenet/main.py
@@ -84,11 +84,11 @@ def triplet_loss(y_true, y_pred, alpha=0.2):
     anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
 
     # Step 1: Compute the (encoding) distance between the anchor and the positive, you will need to sum over axis=-1
-    # pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), axis=-1) # correct version
-    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)))  # dummy version to validate grade
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), axis=-1) # correct version
+    # pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)))  # dummy version to validate grade
     # Step 2: Compute the (encoding) distance between the anchor and the negative, you will need to sum over axis=-1
-    # neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), axis=-1) # correct version
-    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)))  # dummy version to validate grade
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), axis=-1) # correct version
+    # neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)))  # dummy version to validate grade
     # Step 3: subtract the two previous distances and add alpha.
     basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
     # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
